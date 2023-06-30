73870996ca92f7ad708283f72141cb1fea77ee27
calcanthum <ivan@iosis-labs.com>
2023-06-30 02:18:40 +0200

    config.py created and key check

--- liv/__init__.py
+++ liv/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
 
     from .views import main
     app.register_blueprint(main)
--- liv/views.py
+++ liv/views.py
@@ -6,6 +6,10 @@ main = Blueprint('main', __name__)
 @main.route('/validate_id', methods=['POST'])
 def validate_id_endpoint():
     data = request.get_json()
+
+    if not data or 'id_number' not in data:
+        return jsonify({"error_message": "Invalid request. No 'id_number' key found in JSON data."}), 400
+
     id_number = data.get('id_number', '')
     result = validate_id(id_number)
     return jsonify(result), 200
